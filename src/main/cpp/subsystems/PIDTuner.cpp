fddcec46d56e654586cec06ada1847bcf5207f0a
thatcomputerguy0101 <thatcomputerguy0101@gmail.com>
2025-12-15 13:13:52 -0500

    Remove redundant return typing in PIDTuner

--- src/main/cpp/subsystems/PIDTuner.cpp
+++ src/main/cpp/subsystems/PIDTuner.cpp
@@ -3,41 +3,41 @@
 void PIDTuner::InitSendable(wpi::SendableBuilder &builder) {
   builder.SetSmartDashboardType("PIDController");
   builder.AddDoubleProperty(
-    "p", [this]() -> double {
+    "p", [this]() {
       return kP;
-    }, [this](double kP) -> void {
+    }, [this](double kP) {
       this->kP = kP;
       handler({PIDUpdate::PIDTerm::kP, kP, slot});
     }
   );
   builder.AddDoubleProperty(
-    "i", [this]() -> double {
+    "i", [this]() {
       return kI;
-    }, [this](double kI) -> void {
+    }, [this](double kI) {
       this->kI = kI;
       handler({PIDUpdate::PIDTerm::kI, kI, slot});
     }
   );
   builder.AddDoubleProperty(
-    "d", [this]() -> double {
+    "d", [this]() {
       return kD;
-    }, [this](double kD) -> void {
+    }, [this](double kD) {
       this->kD = kD;
       handler({PIDUpdate::PIDTerm::kD, kD, slot});
     }
   );
   builder.AddDoubleProperty(
-    "f", [this]() -> double {
+    "f", [this]() {
       return kFF;
-    }, [this](double kFF) -> void {
+    }, [this](double kFF) {
       this->kFF = kFF;
       handler({PIDUpdate::PIDTerm::kFF, kFF, slot});
     }
   );
   builder.AddIntegerProperty(
-    "slot", [this]() -> int {
+    "slot", [this]() {
       return slot;
-    }, [this](int newSlot) -> void {
+    }, [this](int newSlot) {
       slot = newSlot;
     }
   );
